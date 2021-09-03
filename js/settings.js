127b9ef13e72b361f86c30aa10b83966f98e02b1
asrul10 <hanafi.asrul@kotaxdev.com>
2021-09-03 15:30:59 +0700

    code: format code

--- js/settings.js
+++ js/settings.js
@@ -1,85 +1,114 @@
-$(document).ready(function(){
-    // Retrieve defaults
-    chrome.storage.sync.get(['toolbar'], function(result) {
-        if (!result.toolbar) {
-            $("#persistent").prop("checked", true);  
-        }
-    });
-    chrome.storage.sync.get(['flip'], function(result) {
-        if (result.flip) {
-            $("#flip-camera").prop("checked", true);  
-        }
-    });
-    chrome.storage.sync.get(['pushtotalk'], function(result) {
-        if (result.pushtotalk) {
-            $("#push-talk").prop("checked", true);  
-        }
-    });
-    
-    // Get available audio devices
-    function getAudio(request) {
-        $("#audio-select").html("<option value='disabled'>"+chrome.i18n.getMessage("disabled")+"</option>");
-        request.audiodevices.forEach(function(device) {
-            $("#audio-select").append("<option value='"+device.id+"'>"+device.label+"</option>")
-        });
-        $("#audio-select").niceSelect();
-        chrome.storage.sync.get(['mic'], function(result) {
-            if (result.mic != 0) {
-                $('#audio-select').val(result.mic).niceSelect('update');
-            } else {
-                $('#audio-select').val($("#audio-select option:nth-child(2)").val()).niceSelect('update');
-            }
-        });
+$(document).ready(function () {
+  // Retrieve defaults
+  chrome.storage.sync.get(["toolbar"], function (result) {
+    if (!result.toolbar) {
+      $("#persistent").prop("checked", true);
     }
-    
-    // Get available camera devices
-    function getCamera(request) {
-        $("#camera-select").html("<option value='disabled'>"+chrome.i18n.getMessage("disabled")+"</option>");
-        request.cameradevices.forEach(function(device) {
-            $("#camera-select").append("<option value='"+device.id+"'>"+device.label+"</option>")
-        });
-        $("#camera-select").niceSelect();
-        chrome.storage.sync.get(['camera'], function(result) {
-            if (result.camera != 0) {
-                $('#camera-select').val(result.camera).niceSelect('update');
-            } else {
-                $('#camera-select').val($("#camera-select option:nth-child(2)").val()).niceSelect('update');
-            }
-        });
+  });
+  chrome.storage.sync.get(["flip"], function (result) {
+    if (result.flip) {
+      $("#flip-camera").prop("checked", true);
     }
-    
-    // Change settings
-    $("#flip-camera").on("change", function(){
-        chrome.storage.sync.set({flip: this.checked});
-        chrome.runtime.sendMessage({type: "flip-camera", enabled:this.checked});
+  });
+  chrome.storage.sync.get(["pushtotalk"], function (result) {
+    if (result.pushtotalk) {
+      $("#push-talk").prop("checked", true);
+    }
+  });
+
+  // Get available audio devices
+  function getAudio(request) {
+    $("#audio-select").html(
+      "<option value='disabled'>" +
+        chrome.i18n.getMessage("disabled") +
+        "</option>"
+    );
+    request.audiodevices.forEach(function (device) {
+      $("#audio-select").append(
+        "<option value='" + device.id + "'>" + device.label + "</option>"
+      );
     });
-    $("#push-talk").on("change", function(){
-        chrome.storage.sync.set({pushtotalk: this.checked});
-        chrome.runtime.sendMessage({type: "push-to-talk", enabled:this.checked});
+    $("#audio-select").niceSelect();
+    chrome.storage.sync.get(["mic"], function (result) {
+      if (result.mic != 0) {
+        $("#audio-select").val(result.mic).niceSelect("update");
+      } else {
+        $("#audio-select")
+          .val($("#audio-select option:nth-child(2)").val())
+          .niceSelect("update");
+      }
     });
-    $("#camera-select").on("change", function(){
-        chrome.runtime.sendMessage({type: "update-camera", id:$("#camera-select").val()});
+  }
+
+  // Get available camera devices
+  function getCamera(request) {
+    $("#camera-select").html(
+      "<option value='disabled'>" +
+        chrome.i18n.getMessage("disabled") +
+        "</option>"
+    );
+    request.cameradevices.forEach(function (device) {
+      $("#camera-select").append(
+        "<option value='" + device.id + "'>" + device.label + "</option>"
+      );
     });
-    $("#audio-select").on("change", function(){
-        chrome.runtime.sendMessage({type: "update-mic", id:$("#audio-select").val()});
+    $("#camera-select").niceSelect();
+    chrome.storage.sync.get(["camera"], function (result) {
+      if (result.camera != 0) {
+        $("#camera-select").val(result.camera).niceSelect("update");
+      } else {
+        $("#camera-select")
+          .val($("#camera-select option:nth-child(2)").val())
+          .niceSelect("update");
+      }
     });
-    $("#persistent").on("change", function(){
-        chrome.storage.sync.set({toolbar: this.checked});
-        chrome.runtime.sendMessage({type: "switch-toolbar", enabled:!this.checked});
+  }
+
+  // Change settings
+  $("#flip-camera").on("change", function () {
+    chrome.storage.sync.set({ flip: this.checked });
+    chrome.runtime.sendMessage({ type: "flip-camera", enabled: this.checked });
+  });
+  $("#push-talk").on("change", function () {
+    chrome.storage.sync.set({ pushtotalk: this.checked });
+    chrome.runtime.sendMessage({ type: "push-to-talk", enabled: this.checked });
+  });
+  $("#camera-select").on("change", function () {
+    chrome.runtime.sendMessage({
+      type: "update-camera",
+      id: $("#camera-select").val(),
     });
-    
-    // Listen for messages
-    chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-        if (request.type == "device-list") {
-            getAudio(request);
-            getCamera(request);
-        }
+  });
+  $("#audio-select").on("change", function () {
+    chrome.runtime.sendMessage({
+      type: "update-mic",
+      id: $("#audio-select").val(),
     });
-    
-    // Localization (strings in different languages)
-    $("#camera-select-label").html(chrome.i18n.getMessage("camera"));
-    $("#flip-label").html(chrome.i18n.getMessage("flip_camera"));
-    $("#mic-label").html(chrome.i18n.getMessage("microphone"));
-    $("#push-label").html(chrome.i18n.getMessage("push_to_talk"));
-    $("#hover-label").html(chrome.i18n.getMessage("only_on_hover"));
+  });
+  $("#persistent").on("change", function () {
+    chrome.storage.sync.set({ toolbar: this.checked });
+    chrome.runtime.sendMessage({
+      type: "switch-toolbar",
+      enabled: !this.checked,
+    });
+  });
+
+  // Listen for messages
+  chrome.runtime.onMessage.addListener(function (
+    request,
+    sender,
+    sendResponse
+  ) {
+    if (request.type == "device-list") {
+      getAudio(request);
+      getCamera(request);
+    }
+  });
+
+  // Localization (strings in different languages)
+  $("#camera-select-label").html(chrome.i18n.getMessage("camera"));
+  $("#flip-label").html(chrome.i18n.getMessage("flip_camera"));
+  $("#mic-label").html(chrome.i18n.getMessage("microphone"));
+  $("#push-label").html(chrome.i18n.getMessage("push_to_talk"));
+  $("#hover-label").html(chrome.i18n.getMessage("only_on_hover"));
 });
